dcaa5d1a857e69d001f85d35e2b379f6b2e2ba26
AnZenkova <nastyonka16@mail.ru>
2022-05-31 19:45:57 +1000

    Liquibase

--- school/src/main/java/ru/hogwarts/school/model/Student.java
+++ school/src/main/java/ru/hogwarts/school/model/Student.java
@@ -13,7 +13,7 @@ public class Student {
     private int age;
 
     @ManyToOne
-    @JoinColumn(name = "faculty_name")
+    @JoinColumn(name = "faculty_id")
     private Faculty faculty;
 
     public Student(Long id, String name, int age) {
